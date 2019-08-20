c5d2cc2cca834e0e6672f23de0efc80c90bea95d
Jayendra Matarage <jayendramatarage@gmail.com>
2019-08-20 22:36:55 +0530

    grading_students

--- grading_students.py
+++ grading_students.py
@@ -3,5 +3,4 @@ grades = [73, 67, 38, 33]
 for x in grades:
     multi = ((int(x / 5) + 1) * 5)
     gap = multi - x
-    #multi if gap < 3 else x if x < 40 else x
     print(multi, x, gap, x if x < 38 else multi if gap < 3  else x)
